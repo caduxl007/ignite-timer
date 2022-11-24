5712ae731f6a8b4f96a166200cd8bd90983671f9
caduxl007 <eduardo2020dev@gmail.com>
2022-11-23 23:35:05 -0300

    chore: config global styles and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div>Oi</div>
+
+      <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
